2aaa2ded1c458df06fb52cb0504d9a1af5f59aed
ygursu <yasemingrs@gmail.com>
2024-02-25 21:51:26 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp90/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedenucgenalani
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int taban, yukseklik, alan;
+
+            Console.WriteLine("Lütfen üçgenin tabanını giriniz.");
+            taban = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Lütfen üçgenin yüksekliğini giriniz.");
+            yukseklik = Int32.Parse(Console.ReadLine());
+
+            alan = (taban * yukseklik) / 2;
+
+            Console.WriteLine("Üçgenin alanı={0}", alan);
+            Console.ReadLine();
+
+
+        }
+    }
+}
